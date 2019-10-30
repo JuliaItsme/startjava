2fa989bff1284bc05b85b0c8a2a59b49bf44d37d
Julia Goryacheva <juligo04@gmail.com>
2019-10-31 00:27:24 +0300

    change spaces

--- Lesson_1/Cycle/Cycle.java
+++ Lesson_1/Cycle/Cycle.java
@@ -1,22 +1,22 @@
 public class Cycle {
-    public static void main(String[] args) {
-    	for (int numberOne= 0; numberOne <=20; numberOne ++) {
+	public static void main(String[] args) {
+		for (int numberOne= 0; numberOne <=20; numberOne ++) {
       		System.out. print(numberOne + " " + "\n");
-    	}
-    	int numberTwo = 6;
-    	while (numberTwo >= -6) {
-      		System.out.print(numberTwo + " " + "\n");
-      		numberTwo = numberTwo-2;
-    	}
-    	int numberThree = 10;
-    	int summa = 0;
-    	do {
-      		if (numberThree % 2 == 0)
-        		numberThree++;
-      		summa = summa + numberThree;
-      		System.out.println(summa + " ");
-      		numberThree = numberThree + 2;
-    	}
-    	while (numberThree < 20);
-  	}
+		}
+		int numberTwo = 6;
+		while (numberTwo >= -6) {
+			System.out.print(numberTwo + " " + "\n");
+			numberTwo = numberTwo-2;
+		}
+		int numberThree = 10;
+		int summa = 0;
+		do {
+			if (numberThree % 2 == 0)
+				numberThree++;
+			summa = summa + numberThree;
+			System.out.println(summa + " ");
+			numberThree = numberThree + 2;
+		}
+		while (numberThree < 20);
+	}
 }
--- Lesson_1/Unicode/Unicode.java
+++ Lesson_1/Unicode/Unicode.java
@@ -1,8 +1,8 @@
 public class Unicode {
 	public static void main(String[] args) {
 		for (int i = 33; i <= 126 ;i++ ) {
-		    char ch = (char) i;
-		    System.out.println("Unicode" + " " + i + " " + ch );
+			char ch = (char) i;
+			System.out.println("Unicode" + " " + i + " " + ch );
 		}
 	}
 }
