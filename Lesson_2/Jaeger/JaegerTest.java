4f050cc612d84c53778f69c7aa0321890eb8636d
Julia Goryacheva <juligo04@gmail.com>
2019-11-20 23:57:24 +0300

    correction

--- Lesson_2/Jaeger/JaegerTest.java
+++ Lesson_2/Jaeger/JaegerTest.java
@@ -1,21 +1,21 @@
 public class JaegerTest {
-	public static void main(String[] args) {
-		Jaeger jaeger = new Jaeger("Gipsy Danger", 3, "USA", 79.2f, 1980, 9);
-		jaeger.drift();
-		jaeger.attackReflection();
+    public static void main(String[] args) {
+        Jaeger jaeger = new Jaeger("Gipsy Danger", 3, "USA", 79.2f, 1980, 9);
+        jaeger.drift();
+        jaeger.attackReflection();
 
-		System.out.println();
+        System.out.println();
 	
         jaeger = new Jaeger("Striker Eureka", 5, "Australia", 76.2f, 1850, 11);
-		jaeger.drift();
-		jaeger.patrol();
+        jaeger.drift();
+        jaeger.patrol();
 
-		System.out.println();
+        System.out.println();
 
-		jaeger = new Jaeger("Cherno Alpha", 1, "Russia", 85.34f, 2412, 6);
-		jaeger.drift();
-		jaeger.patrol();
-		jaeger.attackReflection();
+        jaeger = new Jaeger("Cherno Alpha", 1, "Russia", 85.34f, 2412, 6);
+        jaeger.drift();
+        jaeger.patrol();
+        jaeger.attackReflection();
 
-	}
+    }
 }
