9b2b3db2b67ccd8907cc3bebd7767b91d1660a4c
Julia Goryacheva <juligo04@gmail.com>
2019-11-20 22:10:12 +0300

    change spaces

--- Lesson_2/Jaeger/Jaeger.java
+++ Lesson_2/Jaeger/Jaeger.java
@@ -1,50 +1,56 @@
 public class Jaeger {
-	private String name;
+    private String name;
     private int mark;
     private String origin;
-	private float height;
-	private float weight;
+    private float height;
+    private float weight;
     private int kaijuKilled;
 
     public String getName() {
         return name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
 
     public int getMark() {
         return mark;
     }
-    public void setMark(int mark){
+
+    public void setMark(int mark) {
         this.mark = mark;
     }
 
     public String getOrigin() {
         return origin;
     }
-    public void setOrigin(String origin){
+
+    public void setOrigin(String origin) {
         this.origin = origin;
     }
 
     public float getHeight() {
         return height;
     }
-    public void setHeight(float height){
+
+    public void setHeight(float height) {
         this.height = height;
     }
 
     public float getWeight() {
         return weight;
     }
-    public void setWeight(float weight){
+
+    public void setWeight(float weight) {
         this.weight = weight;
     }
 
     public int getKaijuKilled() {
         return kaijuKilled;
     }
-    public void setKaijuKilled(int kaijuKilled){
+
+    public void setKaijuKilled(int kaijuKilled) {
         this.kaijuKilled = kaijuKilled;
     }
 
@@ -56,8 +62,8 @@ public class Jaeger {
         System.out.println("Jaeger " + name + " отправлен отражать аттаку Кайдзю");
     }
 
-	public boolean drift() {
+    public boolean drift() {
         System.out.println("Jaeger " + name + " вошел в Дрифт");
-		return true;
-	}
+        return true;
+    }
 }
