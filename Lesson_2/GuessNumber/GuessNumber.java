75cb01676ad7ffe93f738ab1fd596f8ef4c0ce19
Julia Goryacheva <juligo04@gmail.com>
2019-11-24 20:33:41 +0300

    error correction

--- Lesson_2/GuessNumber/GuessNumber.java
+++ Lesson_2/GuessNumber/GuessNumber.java
@@ -1,13 +1,49 @@
-public class GuessNumber {
-    public void guessingNumber(int number) {
-        int numberComp = (0 + (int) (Math.random() * 100));
+import java.util.Scanner;
 
-        if (number > numberComp) {
-            System.out.println("Загаданное игроком число " + number + " больше, чем загаданное компьютером число " + numberComp);
-        } else if (number < numberComp) {
-            System.out.println("Загаданное игроком число " + number + " меньше, чем загаданное компьютером число " + numberComp);
-        } else {
-            System.out.println("Поздравляем! Вы победили!");
-        }
+public class GuessNumber {
+    public void guessingNumber(String playerOne, String playerTwo) {
+      	Scanner scanner = new Scanner(System.in);
+      	Player numbers = new Player();
+    	String answer;
+
+      	do{
+      		int numberComp = (0 + (int) (Math.random() * 100));
+      		int numberOne;
+    		int numberTwo;
+
+      		do{
+        		System.out.print("Iгрок " + playerOne + ", введите число: ");
+          		numbers.setNumber(scanner.nextInt());
+            	numberOne = numbers.getNumber();
+
+	        	if (numberOne > numberComp) {
+	            	System.out.println("Ваше число больше, чем загаданное компьютером");
+	        	} else if (numberOne < numberComp) {
+	            	System.out.println("Ваше число меньше, чем загаданное компьютером");
+	        	} else {
+	        		System.out.println("Поздравляем!" + " " + playerOne + " " + "Вы победили!");
+	        		break;
+	        	}
+
+	        	System.out.print("Iгрок " + playerTwo + ", введите число: ");
+	        	numbers.setNumber(scanner.nextInt());
+	        	numberTwo = numbers.getNumber();
+
+	        	if (numberTwo > numberComp) {
+	            	System.out.println("Ваше число больше, чем загаданное компьютером");
+	        	} else if (numberTwo < numberComp) {
+	            	System.out.println("Ваше число меньше, чем загаданное компьютером");
+	        	} else {
+	        		System.out.println("Поздравляем!" + " " + playerTwo + " " + "Вы победили!");
+	        		break;
+	        	}
+
+        	} while (numberOne != numberComp && numberTwo != numberComp);
+
+          	do {
+                System.out.print("Хотите продолжить? [yes/no]: ");
+                answer = scanner.next();
+            } while (!answer.equals("yes") && !answer.equals("no"));
+        } while (answer.equals("yes"));
     }
 }
--- Lesson_2/GuessNumber/Player.java
+++ Lesson_2/GuessNumber/Player.java
@@ -2,8 +2,18 @@ public class Player {
 
 	private String name;
 	private int number;
-	public Player(String name, int number) {
+
+  	public Player() {}
+
+	public Player(String name) {
 		this.name = name;
+	}
+
+  	public String getName() {
+		return name;
+	}
+
+	public void setNumber(int number) {
 		this.number = number;
 	}
 
