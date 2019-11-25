920d6ed39c7ec065321c0a55110e640e1f5f5e84
Julia Goryacheva <juligo04@gmail.com>
2019-11-25 23:32:28 +0300

    space correction

--- Lesson_2/GuessNumber/GuessNumberTest.java
+++ Lesson_2/GuessNumber/GuessNumberTest.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class GuessNumberTest {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		String answer;
 
 		System.out.print("Введите имя первого игрока: ");
 		Player playerOne = new Player(scanner.next());
@@ -11,6 +12,15 @@ public class GuessNumberTest {
 		Player playerTwo = new Player(scanner.next());
 
 		GuessNumber guessNumber = new GuessNumber(playerOne, playerTwo);
-		guessNumber.guessingNumber();
+
+		do {
+			guessNumber.guessingNumber();
+
+			do {
+				System.out.print("Хотите продолжить? [yes/no]: ");
+				answer = scanner.next();
+			} while (!answer.equals("yes") && !answer.equals("no"));
+			
+		} while (answer.equals("yes"));
 	}
 }
